cae1bb5c483c04e605d20c42c4b3046f0d67c3c1
Daniel Corrêa <daniel.exatas@gmail.com>
2024-07-27 18:03:49 -0300

    Criacao da base do DbContext, fazendo tambem o mapeamento com fluent api das tabelas

---
+++ TechChallenge/src/TechChallenge.Infrastructure/Data/Mapping/ContatoMapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechChallenge.Domain.Entities;
+
+namespace TechChallenge.Infrastructure.Data.Mapping
+{
+    public class ContatoMapping : IEntityTypeConfiguration<Contato>
+    {
+        public void Configure(EntityTypeBuilder<Contato> builder)
+        {
+            builder.HasKey(contato => contato.Id);
+
+            builder.OwnsOne(contato => contato.Regiao, regiao =>
+            {
+                regiao.Property(regiao => regiao.Estado)
+                    .IsRequired()
+                    .HasColumnType("VARCHAR(2)")
+                    .HasColumnName("Estado");
+
+                regiao.Property(regiao => regiao.DDD)
+                    .IsRequired()
+                    .HasColumnName("DDD");
+            });
+
+            builder.OwnsOne(contato => contato.Telefone, telefone =>
+            {
+                telefone.Property(telefone => telefone.Numero)
+                    .IsRequired()
+                    .HasColumnType("VARCHAR(9)")
+                    .HasColumnName("Telefone");
+            });
+
+            builder.OwnsOne(contato => contato.Email, email =>
+            {
+                email.Property(email => email.EnderecoDeEmail)
+                    .IsRequired()
+                    .HasColumnType("VARCHAR(100)")
+                    .HasColumnName("Email");
+            });
+
+            builder.ToTable("Contatos");
+        }
+    }
+}
---
+++ TechChallenge/src/TechChallenge.Infrastructure/Data/TechChallengeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechChallenge.Domain.Entities;
+
+namespace TechChallenge.Infrastructure.Data
+{
+    public class TechChallengeDbContext : DbContext
+    {
+        public TechChallengeDbContext(DbContextOptions<TechChallengeDbContext> options) : base(options) { }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TechChallengeDbContext).Assembly);
+        }
+    }
+}
