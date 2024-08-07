04106cf77deda8f41330a1c06df18dac90885fc4
Daniel Corrêa <daniel.exatas@gmail.com>
2024-08-07 08:42:03 -0300

    Testes com a classe de Regiao

--- TechChallenge/src/TechChallenge.Domain/ValueObjects/Regiao.cs
+++ TechChallenge/src/TechChallenge.Domain/ValueObjects/Regiao.cs
@@ -10,6 +10,8 @@ namespace TechChallenge.Domain.ValueObjects
         {
             Estado = estado;
             DDD = ddd;
+
+            Validar();
         }
 
         protected Regiao() { }
---
+++ TechChallenge/src/TechChallenge.Testes/Domain/ValueObjects/RegiaoTestes.cs
@@ -0,0 +1,29 @@
+﻿using TechChallenge.Domain.Enum;
+using TechChallenge.Domain.Exceptions;
+using TechChallenge.Domain.ValueObjects;
+
+namespace TechChallenge.Testes.Domain.ValueObjects
+{
+    public class RegiaoTestes
+    {
+        [Fact]
+        [Trait("Regiao", "Deve conseguir criar região normalmente")]
+        public void Regiao_InstanciaRegiao_DeveConseguirCriarRegiaoComSucesso()
+        {
+            // Arrange
+            var regiao = new Regiao(Estado.SP, 11);
+
+            // Act & Assert
+            Assert.NotNull(regiao);
+        }
+
+        [Theory]
+        [InlineData(Estado.SP, 31)]
+        [InlineData(Estado.RJ, 11)]
+        public void Regiao_InstanciaRegiao_DeveDarErroPorRegiaoComDDDInvalido(Estado estado, int ddd)
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<RegiaoComDDDInvalidoException>(() => new Regiao(estado, ddd));
+        }
+    }
+}
