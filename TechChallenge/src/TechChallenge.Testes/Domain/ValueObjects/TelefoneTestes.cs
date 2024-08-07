7c3fae11deec05836235386aa4d63f4e97db3fac
Daniel Corrêa <daniel.exatas@gmail.com>
2024-08-07 08:48:24 -0300

    Adicionando testes de unidade para o telefone

---
+++ TechChallenge/src/TechChallenge.Testes/Domain/ValueObjects/TelefoneTestes.cs
@@ -0,0 +1,30 @@
+﻿using TechChallenge.Domain.ValueObjects;
+using TechChallenge.SharedKernel.Validations;
+
+namespace TechChallenge.Testes.Domain.ValueObjects
+{
+    public class TelefoneTestes
+    {
+        [Fact]
+        [Trait("Telefone", "Deve conseguir criar telefone normalmente")]
+        public void Telefone_Construtor_DeveConseguirInstanciarTelefoneNormalmente()
+        {
+            // Arrange
+            var telefone = new Telefone("1111-1111");
+
+            // Act & Assert
+            Assert.NotNull(telefone);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("11111111")]
+        [InlineData("abcd-abcd")]
+        [Trait("Telefone", "Deve dar erro ao criar o telefone")]
+        public void Telefone_Construtor_DeveDarErroAoRealizarInstanciaDeTelefone(string numeroTelefone)
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<DomainException>(() => new Telefone(numeroTelefone));
+        }
+    }
+}
