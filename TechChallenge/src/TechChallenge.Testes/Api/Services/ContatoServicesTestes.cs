c02117106bce61ac4a5115c203d638a427f8b4e5
Daniel Corrêa <daniel.exatas@gmail.com>
2024-08-07 10:49:09 -0300

    Realizado testes para o service de contato no método de atualizar contato

--- TechChallenge/src/TechChallenge.Testes/Api/Services/ContatoServicesTestes.cs
+++ TechChallenge/src/TechChallenge.Testes/Api/Services/ContatoServicesTestes.cs
@@ -1,15 +1,26 @@
 ﻿using Moq;
 using TechChallenge.Api.DTO.Request;
 using TechChallenge.Api.Services;
+using TechChallenge.Domain.Entities;
 using TechChallenge.Domain.Repository;
+using TechChallenge.Testes.Api.Services.Fixtures;
+using static TechChallenge.Testes.Api.Services.Fixtures.ContatoServicesTestesFixture;
 
 namespace TechChallenge.Testes.Api.Services
 {
+    [Collection(nameof(ContatoServicesTestesCollection))]
     public class ContatoServicesTestes
     {
+        private readonly ContatoServicesTestesFixture _contatoServicesTestesFixture;
+
+        public ContatoServicesTestes(ContatoServicesTestesFixture contatoServicesTestesFixture)
+        {
+            _contatoServicesTestesFixture = contatoServicesTestesFixture;
+        }
+
         [Fact]
-        [Trait("ContatoService", "Deve retrnar que a requisicao nao esta valida")]
-        public async void ContatoService_AtualizarContato_DeveRetornarRequisicaoInvalida()
+        [Trait("ContatoServices", "Deve retrnar que a requisicao nao esta valida")]
+        public async void ContatoServices_AtualizarContato_DeveRetornarRequisicaoInvalida()
         {
             // Arrange
             var contatoRepository = new Mock<IContatoRepository>();
@@ -31,6 +42,69 @@ namespace TechChallenge.Testes.Api.Services
             // Assert
             Assert.Equal(4, validationResult.Errors.Count);
             Assert.False(validationResult.IsValid);
+        }
+
+        [Fact]
+        [Trait("ContatoServices", "Deve retornar que o contato não existe ao tentar realizar atualização")]
+        public async void ContatoServices_AtualizarContato_DeveRetornarErroDeContatoInexistente()
+        {
+            // Arrange
+            var contatoRepository = new Mock<IContatoRepository>();
+            contatoRepository
+                .Setup(repository => repository.BuscarPorId(It.IsAny<Guid>()))
+                .ReturnsAsync((Contato?)null);
+
+
+            var request = new AtualizarContatoRequest()
+            {
+                Nome = "Daniel",
+                Telefone = "1111-1111",
+                Email = "daniel@email.com.br",
+                Estado = "SP",
+                DDD = 11
+            };
+
+            var contatoServices = new ContatoServices(contatoRepository.Object);
+
+            // Act
+            var validationResult = await contatoServices.AtualizarContato(Guid.NewGuid(), request);
+
+            // Assert
+            Assert.Single(validationResult.Errors);
+            Assert.False(validationResult.IsValid);
+        }
+
+        [Fact]
+        [Trait("ContatoServices", "Deve retornar que conseguiu atualizar contato com sucesso")]
+        public async void ContatoServices_AtualizarContato_DeveConseguirAtualizarContatoComSucesso()
+        {
+            // Arrange
+            var contatoRepository = new Mock<IContatoRepository>();
+            contatoRepository
+                .Setup(repository => repository.BuscarPorId(It.IsAny<Guid>()))
+                .ReturnsAsync(_contatoServicesTestesFixture.CriaContatoValido());
+
+            contatoRepository
+                .Setup(repository => repository.UnitOfWork.ConfirmarTransacao())
+                .ReturnsAsync(true);
+
+            var request = new AtualizarContatoRequest()
+            {
+                Nome = "Daniel",
+                Telefone = "1111-1111",
+                Email = "daniel@email.com.br",
+                Estado = "SP",
+                DDD = 11
+            };
+
+            var contatoServices = new ContatoServices(contatoRepository.Object);
+
+            // Act
+            var validationResult = await contatoServices.AtualizarContato(Guid.NewGuid(), request);
+
+            // Assert
+            Assert.Empty(validationResult.Errors);
+            Assert.True(validationResult.IsValid);
         }
     }
 }
---
+++ TechChallenge/src/TechChallenge.Testes/Api/Services/Fixtures/ContatoServicesTestesFixture.cs
@@ -0,0 +1,23 @@
+﻿using TechChallenge.Domain.Entities;
+using TechChallenge.Domain.Enum;
+using TechChallenge.Domain.ValueObjects;
+
+namespace TechChallenge.Testes.Api.Services.Fixtures
+{
+    public class ContatoServicesTestesFixture : IDisposable
+    {
+        [CollectionDefinition(nameof(ContatoServicesTestesCollection))]
+        public class ContatoServicesTestesCollection : ICollectionFixture<ContatoServicesTestesFixture> { }
+
+        public Contato CriaContatoValido()
+        {
+            var telefone = new Telefone("1111-1111");
+            var email = new Email("daniel@email.com");
+            var regiao = new Regiao(Estado.SP, 11);
+
+            return new Contato("Daniel", telefone, email, regiao);
+        }
+
+        public void Dispose() { }
+    }
+}
