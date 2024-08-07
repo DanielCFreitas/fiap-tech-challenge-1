b139f488d72ad9f6668b822e1accf05c094cc230
Daniel Corrêa <daniel.exatas@gmail.com>
2024-08-07 08:12:55 -0300

    Testes realizados para validacao de endereco de email

---
+++ TechChallenge/src/TechChallenge.Testes/Domain/ValueObjects/EmailTestes.cs
@@ -0,0 +1,33 @@
+﻿using TechChallenge.Domain.ValueObjects;
+using TechChallenge.SharedKernel.Validations;
+
+namespace TechChallenge.Testes.Domain.ValueObjects
+{
+    public class EmailTestes
+    {
+        [Theory]
+        [InlineData("teste@contato.com")]
+        [InlineData("teste@contato.com.br")]
+        [InlineData("teste@contato.edu.br")]
+        [Trait("Email", "Deve conseguir criar e-mail válido")]
+        public void Email_Construtor_DeveConseguirCriarValueObjectDeEmail(string enderecoDeEmail)
+        {
+            // Arrange
+            var email = new Email(enderecoDeEmail);
+
+            // Act & Assert
+            Assert.NotNull(email);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("email")]
+        [InlineData("emailcontato.com.br")]
+        [InlineData("email@contato")]
+        public void Email_Construtor_DeveEstourarExcecaoDeEmailInvalido(string enderecoDeEmail)
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<DomainException>(() => new Email(enderecoDeEmail));
+        }
+    }
+}
